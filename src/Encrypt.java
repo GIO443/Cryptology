4ab5c5a2a60bb7e092385b3d02f00f502706bbad
GIO443 <62480056+GIO443@users.noreply.github.com>
2022-05-23 22:37:55 -0700

    hrnnn

---
+++ src/Encrypt.java
@@ -0,0 +1,18 @@
+public class Encrypt {
+    String message;
+    String[][] alphabet; //reference of letter to number
+
+
+    public Encrypt(String passedMessage){
+        this.message = passedMessage;
+    }
+
+    public int toEncrypt(String message){
+        return 0;
+    }
+
+    public int[] changeToNum(String message){
+        int[] intArray = new int[message.length()];
+        return intArray;
+    }
+}
--- src/KeyGenerator.java
+++ src/KeyGenerator.java
@@ -1,3 +1,5 @@
 public class KeyGenerator {
+    //generator random key
+    int key;
 
 }
