f1f19bcaebeb0d6f38534f7cc5f09c77e0f107a1
GIO443 <62480056+GIO443@users.noreply.github.com>
2022-05-23 23:41:09 -0700

    hrnnn

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -1,6 +1,11 @@
 public class Encrypt {
     String message;
-    String[][] alphabet; //reference of letter to number
+    String[][] alphabet = {{"A", "1"}, {"B", "2"}, {"C", "3"}, {"D", "4"},
+            {"E", "5"}, {"F", "6"},{"G", "7"}, {"H", "8"}, {"I", "9"},
+            {"J", "10"}, {"K", "11"}, {"L", "12"}, {"M", "13"}, {"N", "14"},
+            {"O", "15"}, {"P", "16"}, {"Q", "17"}, {"R", "18"}, {"S", "19"},
+            {"T", "20"}, {"U", "21"}, {"V", "22"}, {"W", "23"}, {"X", "24"},
+            {"Y5", "2"}, {"Z", "26"}}; //reference of letter to number
 
 
     public Encrypt(String passedMessage){
